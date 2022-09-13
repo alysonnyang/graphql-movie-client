aa1139d8ffd382f9c8b0d14c96fe1b08b9857a97
alysonnyang <alyson.n.yang@gmail.com>
2022-09-13 18:43:28 +0900

    graphql movie app - set environment
    connect to server, create Routes

---
+++ src/routes/Movies.js
@@ -0,0 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  {
+    allMovies {
+      title
+      summary
+      id
+    }
+  }
+`;
+
+export default function Movies() {
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading . . . </h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch! </h1>;
+  }
+  return (
+    <ul>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>
+          <h3>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
